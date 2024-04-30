4ab7750a1a5ccc2ba75f8ce36914a3728f57f748
GregoryRobetertson <geefromla@gmail.com>
2024-04-30 10:37:10 -0500

    Created component to sign in with google auth

---
+++ src/app/GoogleSignIn.jsx
@@ -0,0 +1,23 @@
+'use clien';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from './firebaseConfig';
+import React from 'react'
+
+export default function GoogleSignIn() {
+   const handGoogleSignIn = async () => {
+    try {
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+        console.log('User is signed in:', user.displayName);
+    } catch (error) {
+        console.error('Error signing in:', error);
+    }
+   } 
+  return (
+    <>
+    <button> Sign in with Google </button>
+    <p>Click the button above to sign in!</p>
+    </>
+    
+  )
+}
