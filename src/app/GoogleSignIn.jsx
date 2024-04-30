eca7ab898cffd1146f483dacaf30641c5ec17aee
GregoryRobetertson <geefromla@gmail.com>
2024-04-30 14:28:46 -0500

    Updated component with logic to signout and styled the button

--- src/app/GoogleSignIn.jsx
+++ src/app/GoogleSignIn.jsx
@@ -1,38 +1,71 @@
-'use client';
-import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from './firebaseConfig';
-import React, { useState } from 'react'
+"use client";
+import { signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
+import { auth, provider } from "./firebaseConfig";
+import React, { useState } from "react";
 
 export default function GoogleSignIn() {
-    const [userObj, setUserObj] = useState(null);
+  const [userObj, setUserObj] = useState(null);
 
-   const handGoogleSignIn = async () => {
+  const handleGoogleSignIn = async () => {
     try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-        console.log('User signed in:', user.displayName);
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      setUserObj(user);
+      console.log("User signed in:", user.displayName);
     } catch (error) {
-        console.error('Error signing in:', error);
+      console.error("Error signing in:", error);
     }
-   } 
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUserObj(null);
+      console.log("User logged out");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   return (
     <>
-    <button onClick={handGoogleSignIn}
-    style={{
-        backgroundColor: '#4285f4',
-        color: 'white',
-        fontFamily: 'Roboto, sans-serif',
-        fontSize: '14px',
-        border: 'none',
-        borderRadius: '4px',
-        padding: '10px 24px',
-        cursor: 'pointer',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.25)', // Adding box shadow
-      }}
-    > Sign in with Google </button>
-    {userObj && <p>Welcome back{userObj.displayName}</p>}
-   {!userObj && <p>Click the button above to sign in!</p>} 
+      {!userObj && (
+        <button
+          onClick={handleGoogleSignIn}
+          style={{
+            backgroundColor: "#4285f4",
+            color: "white",
+            fontFamily: "Roboto, sans-serif",
+            fontSize: "14px",
+            border: "none",
+            borderRadius: "4px",
+            padding: "10px 24px",
+            cursor: "pointer",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.25)", // Adding box shadow
+          }}
+        >
+          Sign in with Google{" "}
+        </button>
+      )}
+      {userObj && (
+        <button
+          onClick={handleLogout}
+          style={{
+            backgroundColor: "#4285f4",
+            color: "white",
+            fontFamily: "Roboto, sans-serif",
+            fontSize: "14px",
+            border: "none",
+            borderRadius: "4px",
+            padding: "10px 24px",
+            cursor: "pointer",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.25)", // Adding box shadow
+          }}
+        >
+          Log out{" "}
+        </button>
+      )}
+      {userObj && <p>Welcome back {userObj.displayName}</p>}
+      {!userObj && <p>Click the button above to sign in!</p>}
     </>
-    
-  )
+  );
 }
